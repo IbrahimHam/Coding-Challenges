de0780af5a5437e23a34fcec7e8c5e39cbfccb51
IbrahimHam <42837548+IbrahimHam@users.noreply.github.com>
2021-09-02 10:18:44 -0700

    Update question1.php

--- question1.php
+++ question1.php
@@ -1,45 +1,72 @@
 <?php
 
-function check_tags($input, $tags){
-    //  check if current_tag is open or close tag
-    //  if open -> old_tag = current_tag
-    //  add tag to the tags array
-    //  if close -> check if current_tag is the closed tag for the old_tag ... if true pop old_tag from tags array and remove current_tag from $input
+    function check_tags($input = null, $stack = array()){
 
-    $tag = get_string_between($input, "<", ">");
-    if(check_open_tags('<'. $tag '/>')){
-        array_push($tags, '<'. $tag '/>')
+        $tag_indexes = get_tags_start_and_end_indexes($input, '<', '>');
+        if (!$tag_indexes){
+            echo "The String is nested Correctly";
+            return;
+        }
+
+        $len = strlen($input) - $tag_indexes['end_index'];
+        $tag = substr($input, $tag_indexes['start_index'], strlen($input) == $tag_indexes['end_index'] + 1 ? strlen($input) :  - $len + 1 );
+        if(check_open_tags($tag)){
+            array_push($stack , $tag);
+        } 
+        else if (check_close_tags($tag)){
+            $open_tag = array_pop($stack);
+            if(!match_dom_elements($open_tag, $tag)){
+                echo substr($open_tag, 1, -1);
+                return;
+            }
+        }
+
+            $sub_ini = substr($input, $tag_indexes['end_index'] + 1, strlen($input));
+            check_tags($sub_ini, $stack);
     }
-    elseif (check_close_tags('</'. $tag '>')) {
-        if()
+
+    function match_dom_elements($open_tag, $close_tag){
+        $tags = array(
+            "<b>" => "</b>",
+            "<i>" => "</i>",
+            "<em>" => "</em>",
+            "<div>" => "</div>",
+            "<p>" => "</p>"
+        );
+        return $close_tag == $tags[$open_tag];
     }
+
+    function get_tags_start_and_end_indexes($string, $start_char, $end_char){
+        if(!empty($string)){
+            $start_index = strpos($string, $start_char);
+            $end_index = strpos($string, $end_char) ;
+
+            if ($start_index === false || $end_index === false)
+                return false;
+            return array('start_index' => $start_index, 'end_index' => $end_index);
+        }
+        return false;
+    }
+
+    function check_open_tags($tag){
+            $open_tags = array('<b>', '<i>', '<em>', '<div>', '<p>');
+            if (in_array($tag, $open_tags))
+                return true;       
+    }
+
+    function check_close_tags($tag){
+        $close_tags = array('</b>', '</i>', '</em>', '</div>', '</p>');
+        if (in_array($tag, $close_tags))
+            return true;       
+    }
+
+
+$examples = array("<div><b><p>hello world</p></b></div>",
+                  "<div><div><b></b></div></p>",
+                  "<div>abc</div><p><em><i>test test test</b></em></p>" ,
+);
+
+foreach ($examples as $example) {
+    echo check_tags($example);
+    echo "<br>";
 }
-
-function get_string_between($string, $start, $end){
-    $string = ' ' . $string;
-    $ini = strpos($string, $start);
-    if ($ini == 0) return '';
-    $ini += strlen($start);
-    $len = strpos($string, $end, $ini) - $ini;
-    return substr($string, $ini, $len);
-}
-
-function check_open_tags($tag){
-    $open_tags = array('<b>', '<i>', '<em>', '<div>', '<p>');
-    if (in_array($tag, $open_tags))
-        return true;       
-}
-
-function check_close_tags($tag){
-$close_tags = array('</b>', '</i>', '</em>', '</div>', '</p>');
-if (in_array($tag, $close_tags))
-    return true;       
-}
-
-$input = "<div><b><p>hello world</p></b></div>";
-$temp = $input;
-$tags = array();
-check_tags($input, $tags);
-
-HTMLElements($input);
-?>
